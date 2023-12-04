1516b77ebb7e63cd6544a871d1c633e920b8e392
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-03 23:31:30 -0600

    added links to logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
 import "../assets/css/footer.css";
+import InstagramLogo from "../assets/img/instagram.svg";
+import GithubLogo from "../assets/img/square-github.svg";
+import LinkedInLogo from "../assets/img/linkedin-animated.svg";
 
 export default function Footer() {
   return (
     <footer>
+      <p className="logos">
+        <a href="https://www.instagram.com/">
+          <img src={InstagramLogo} alt="Instagram Logo"></img>
+        </a>
+        <a href="https://github.com/">
+          <img src={GithubLogo} alt="Github Logo"></img>
+        </a>
+        <a href="https://www.linkedin.com/">
+          <img src={LinkedInLogo} alt="Linkedin Logo"></img>
+        </a>
+      </p>
       <p className="copyright">
         Copyright 2023 Laura Vollmer. All rights reserved.
       </p>
