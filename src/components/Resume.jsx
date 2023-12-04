48500e92681a29b14c6b9c28a2ce51760d06dab7
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-04 11:38:46 -0600

    styling button and updated developer proficiences

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,20 +4,43 @@ export default function Resume() {
   return (
     <>
       <div className="technical">
-        <h2>Developer's Technical Proficiences:</h2>
+        <h1>Developer's Technical Proficiences:</h1>
         <p>
           <ul>
-            <li>CSS</li>
-            <li>HTML</li>
-            <li>React</li>
-            <li>Javascript</li>
+            <li>
+              Build front-end websites from scratch, and ready-made website
+              frameworks
+            </li>
+            <li>
+              Create full-stack single page website applications using RESTful
+              API routes and understand the communication with backend servers
+            </li>
+            <li>
+              Implement structured and unstructured database to convert static
+              websites into dynamic websites that persist data{" "}
+            </li>
+            <li>
+              Apply the standard basics of social coding such as source control,
+              issue tracking and mergining branches
+            </li>
+            <li>
+              Demonstrate strong teamwork and communication skills with other
+              students
+            </li>
+            <li>CSS, HTML, Javascript, Reat</li>
           </ul>
         </p>
       </div>
-      <h2>Please check out my resume for more information!</h2>
-      <a href="/documents/resume.pdf" download="resume.pdf" className="resume">
-        Download Resume Here
-      </a>
+      <h1>Please check out my resume for more information!</h1>
+      <button>
+        <a
+          href="/documents/resume.pdf"
+          download="resume.pdf"
+          className="resume"
+        >
+          Download Resume Here
+        </a>
+      </button>
     </>
   );
 }
