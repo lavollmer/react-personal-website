c380c66a6d8487b9d5590b3255efb16006aa5bea
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-04 10:41:04 -0600

    fixing navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,36 +8,52 @@ import "../assets/css/header.css";
 //put navigation bar into header
 export default function Header() {
   const whiteBackground = {
-    backgroundColor: "white",
+    backgroundColor: "black",
     width: "100vw",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const navStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    margin: "0px",
+    padding: "0px",
   };
 
   return (
     <header>
       <Navbar style={whiteBackground}>
         <Container className="navbar-container">
-          <h1>Laura Vollmer</h1>
-          <ul className="navbar-links">
+          <div style={navStyle}>
+            <h1>Laura Vollmer</h1>
+          </div>
+          <ul className="navbar-links" style={navStyle}>
             <Link to="/" className="link-style">
               Home
             </Link>
           </ul>
-          <ul className="navbar-links">
+          <ul className="navbar-links" style={navStyle}>
             <Link to="/about" className="link-style">
               About
             </Link>
           </ul>
-          <ul className="navbar-links">
+          <ul className="navbar-links" style={navStyle}>
             <Link to="/contactme" className="link-style">
               Contact
             </Link>
           </ul>
-          <ul className="navbar-links">
+          <ul className="navbar-links" style={navStyle}>
             <Link to="/project" className="link-style">
               Project
             </Link>
           </ul>
-          <ul className="navbar-links">
+          <ul className="navbar-links" style={navStyle}>
             <Link to="/resume" className="link-style">
               Resume
             </Link>
