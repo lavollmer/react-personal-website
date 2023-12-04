bff8ea725a2eb56c6fda2931e4cec0c990d3c5ea
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-03 18:31:13 -0600

    practicing coding react

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 export default function Contact() {
+  const [contactInfo, setContactInfo] = useState("laura");
+
+  function handleInputChange(event) {
+    setContactInfo(event.target.value);
+  }
+
   return (
     <>
       <div>
         <p>
-          Contact me for more information. Please enter your information below:{" "}
+          Contact me for more information. Please enter your information below:
         </p>
       </div>
       <form>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={contactInfo}
+          onChange={handleInputChange}
+        />
       </form>
+
+      <p>Right now, your name is {contactInfo}.</p>
     </>
   );
 }
