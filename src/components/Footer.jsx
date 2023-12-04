028c80e4f72efd72deba7977f19b1b242c97f734
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-04 12:07:05 -0600

    fixed footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,17 +6,25 @@ import LinkedInLogo from "../assets/img/linkedin.svg";
 export default function Footer() {
   return (
     <footer>
-      <p className="logos">
+      <div className="logos">
         <a href="https://www.instagram.com/">
-          <img src={InstagramLogo} alt="Instagram Logo"></img>
+          <img
+            className="logoimage"
+            src={InstagramLogo}
+            alt="Instagram Logo"
+          ></img>
         </a>
         <a href="https://github.com/">
-          <img src={GithubLogo} alt="Github Logo"></img>
+          <img className="logoimage" src={GithubLogo} alt="Github Logo"></img>
         </a>
         <a href="https://www.linkedin.com/">
-          <img src={LinkedInLogo} alt="Linkedin Logo"></img>
+          <img
+            className="logoimage"
+            src={LinkedInLogo}
+            alt="Linkedin Logo"
+          ></img>
         </a>
-      </p>
+      </div>
       <p className="copyright">
         Copyright 2023 Laura Vollmer. All rights reserved.
       </p>
